285745b5a95222548a54b42c349224e790e4ba46
GrapeJuiceSoda <kendallcarmel@gmail.com>
2023-06-07 18:04:18 -0700

    getting started

--- cat-cc.cpp
+++ cat-cc.cpp
@@ -1,39 +1,71 @@
+#include <iostream>
+#include <string>
+
 #include <tao/pegtl.hpp>
 
-using namespace tao::pegtl;
+namespace pegtl = tao::pegtl;
 
-namespace modulus
+namespace hello
 {
-   template< unsigned M, unsigned R = 0 >
-   struct my_rule
-   {
-      static_assert( M > 1, "Modulus must be greater than 1" );
-      static_assert( R < M, "Remainder must be less than modulus" );
+   // Parsing rule that matches a literal "Hello, ".
 
+   struct prefix
+      : pegtl::string< 'H', 'e', 'l', 'l', 'o', ',', ' ' >
+   {};
+
+   // Parsing rule that matches a non-empty sequence of
+   // alphabetic ascii-characters with greedy-matching.
+
+   struct name
+      : pegtl::plus< pegtl::alpha >
+   {};
+
+   // Parsing rule that matches a sequence of the 'prefix'
+   // rule, the 'name' rule, a literal "!", and 'eof'
+   // (end-of-file/input), and that throws an exception
+   // on failure.
+
+   struct grammar
+      : pegtl::must< prefix, name, pegtl::one< '!' >, pegtl::eof >
+   {};
+
+   // Class template for user-defined actions that does
+   // nothing by default.
+
+   template< typename Rule >
+   struct action
+   {};
+
+   // Specialisation of the user-defined action to do
+   // something when the 'name' rule succeeds; is called
+   // with the portion of the input that matched the rule.
+
+   template<>
+   struct action< name >
+   {
       template< typename ParseInput >
-      static bool match( ParseInput& in )
+      static void apply( const ParseInput& in, std::string& v )
       {
-         if( !in.empty() ) {
-            if( ( ( *in.current() ) % M ) == R ) {
-               in.bump( 1 );
-               return true;
-            }
-         }
-         return false;
+         v = in.string();
       }
    };
 
-   struct grammar
-      : until< eolf, must< my_rule< 3 > > >
-   {};
+}  // namespace hello
 
-}  // namespace modulus
-
-int main( int argc, char** argv )
+int main( int argc, char* argv[] )
 {
-   if( argc > 1 ) {
-      argv_input in( argv, 1 );
-      parse< modulus::grammar >( in );
-   }
+   if( argc != 2 )
+      return 1;
+
+   // Start a parsing run of argv[1] with the string
+   // variable 'name' as additional argument to the
+   // action; then print what the action put there.
+
+   std::string name;
+
+   pegtl::argv_input in( argv, 1 );
+   pegtl::parse< hello::grammar, hello::action >( in, name );
+
+   std::cout << "Good bye, " << name << "!" << std::endl;
    return 0;
 }
